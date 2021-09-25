10497a15c5324cba77b8a85440d17078e24384af
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 22:16:10 +0530

    Create operations.java

---
+++ arrays/operations.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class operations {
+    public static void main(String[]args){
+       int a=Integer.parseInt(args[0]);
+       int b=Integer.parseInt(args[1]);
+        System.out.println("The value of a is"+(a)+"the value of b is"+(b));
+        System.out.println("The result of a+b is="+(a+b));
+        System.out.println("The result of a-b is="+(a-b));
+        System.out.println("The result of a*b is="+(a*b));
+        System.out.println("The result of a/b is="+(a/b));
+        System.out.println("The result of a%b is="+(a%b));
+
+    }
+}
