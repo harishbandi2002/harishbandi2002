cd7f53707735ff134507a37925feb82f2d5fead5
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 20:03:18 +0530

    Create Arraysint.java

---
+++ arrays/Arraysint.java
@@ -0,0 +1,17 @@
+package com.java.java;
+
+public class Arraysint {
+    public static void main(String[] args) {
+        {
+            int[] array;
+            array = new int[10];
+            array[0] =12;
+            array[9] =99;
+            System.out.println("Element at index0:" + array[0]);
+            System.out.println("Element at index0:" + array[1]);
+            System.out.println("Element at index0:" + array[9]);
+
+
+        }
+    }
+}
