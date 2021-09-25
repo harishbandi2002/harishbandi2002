3b6bbc9060afa305b3fc6aa47b87e0e5490eba04
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 22:10:02 +0530

    Create Strings.java

---
+++ arrays/Strings.java
@@ -0,0 +1,18 @@
+public class Strings {
+    public static void main(String[]args){
+        //Strings
+        //concatenate
+        String name="Mounish";
+        String name1="Allam";
+        String name3=name+" "+name1;
+        System.out.println(name3);
+        //charAt
+        System.out.println(name.charAt(1));
+        //length
+        System.out.println(name.length());
+        //replace
+        System.out.println(name.replace('M','L'));
+        //Substring
+        System.out.println(name.substring(5,7));
+    }
+}
