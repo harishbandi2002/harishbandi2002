09c5e5756ccd8ce7b0ebd80e8ff62c24cdd78c36
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 20:02:37 +0530

    Create Arraysfloat.java

---
+++ arrays/Arraysfloat.java
@@ -0,0 +1,19 @@
+package com.java.java;
+
+public class Arraysfloat {
+    public static void main(String[]args){
+        {
+            float[]array;
+            array=new float[10];
+            array[0]=(float) 100.12;
+            array[9]=(float) 200.99;
+            System.out.println("Element at index0:"+ array[0]);
+            System.out.println("Element at index0:"+ array[1]);
+            System.out.println("Element at index0:"+ array[9]);
+
+
+
+
+        }
+    }
+}
