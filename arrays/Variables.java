704362c04f4d28bd6883b6b27f61c7d78d43e192
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 22:11:09 +0530

    Create Variables.java

---
+++ arrays/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        //Variables
+        String name="Mounish";
+        int age=99;
+        String neighbour="Appu";
+        String friend=name;
+        System.out.println(name);
+        System.out.println(neighbour);
+        System.out.println(age);
+        System.out.println(friend+neighbour);
+    }
+}
