1112858021e85356de6090b5427283df8abd45ad
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 20:12:29 +0530

    Create FindArguments.java

---
+++ arrays/FindArguments.java
@@ -0,0 +1,14 @@
+package com.java.java;
+
+public class FindArguments {
+    public static void main(String[]args){
+        {
+                System.out.println("TheNumber of Arguments are:"+args.length);
+            System.out.println("The Following are the Arguments:");
+                System.out.println("1."+args[0]);
+                System.out.println("2."+args[1]);
+                System.out.println("3."+args[2]);
+            }
+    }
+
+}
