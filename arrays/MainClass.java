3719f85ad7c0a7735d57da1b2d2e9aea72949a44
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 20:16:40 +0530

    Create MainClass.java

---
+++ arrays/MainClass.java
@@ -0,0 +1,21 @@
+class subClass
+{
+	String msg = "Hello Welcome to Classes";
+	void display()
+	{
+		System.out.println(msg);	
+	}
+}
+
+class MainClass
+{
+	public static void main(String args[])	
+	{
+		subClass sc = new subClass();
+		sc.display();	
+
+
+
+
+	}
+}
