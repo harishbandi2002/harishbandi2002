8258d67c74b93f35323fa15c6da502ae0bb47af3
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 19:56:46 +0530

    Create ArithmeticOperetors.java

---
+++ arrays/ArithmeticOperetors.java
@@ -0,0 +1,24 @@
+package com.Kluniversity;
+
+public class ArithmeticOperetors {
+    public static void main(String[]args){
+        //Arithmetic ArithmeticOperetors
+        int a=10;
+        int b=20;
+        int sum= a + b;
+        int sub= a - b;
+        int mul= a * b;
+        int div= a / b;
+        int mod= a % b;
+        System.out.println("The value of a is:"+a);
+        System.out.println("The value of b is:"+b);
+        System.out.println("a+b:"+sum);
+        System.out.println("a-b:"+sub);
+        System.out.println("a*b:"+mul);
+        System.out.println("a/b:"+div);
+        System.out.println("a%b:"+mod);
+
+
+    }
+}
+© 2021 GitHub, Inc.
