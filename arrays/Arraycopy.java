56549ee3cb515253f4b52ad59bdda80b82a4f977
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 19:59:53 +0530

    Create Arraycopy.java

---
+++ arrays/Arraycopy.java
@@ -0,0 +1,10 @@
+package com.java.java;
+public class Arraycopy {
+    public static void main(String[]args){
+        int[] source = {100, 200, 300};
+        int[]	dest = new int[3];
+        System.arraycopy(source, 0, dest, 0, source.length);
+        for (int i =0;	i <dest.length;	i++)
+            System.out.println("Element at index " + i + ": " +dest[i]);
+    }
+}
