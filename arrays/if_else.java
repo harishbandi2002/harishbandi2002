2ced4b9b1e5e8a6adafdf824ca0a844ea9957d86
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 22:13:21 +0530

    Create if_else.java

---
+++ arrays/if_else.java
@@ -0,0 +1,13 @@
+package com.java.java;
+
+public class if_else {
+    public static void main(String[] args) {
+        int a=Integer.parseInt(args[0]);
+        if(a%2==0){
+            System.out.println("The given number is even");
+        }
+        else{
+            System.out.println("The given number is odd");
+        }
+    }
+}
