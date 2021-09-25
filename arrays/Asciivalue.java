6403f6c141a105dc42b65caf8671710c8bcdb9c6
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 20:04:32 +0530

    Create Asciivalue.java

---
+++ arrays/Asciivalue.java
@@ -0,0 +1,12 @@
+package com.java.java;
+
+public class Asciivalue {
+    public static void main(String[]args){
+        char ch1 = 'a';
+        char ch2 = 'b';
+        int ascii1 = ch1;
+        int ascii2 = ch2;
+        System.out.println("The ASCII value of " + ch1 + " is: " + ascii1);
+        System.out.println("The ASCII value of " + ch2 + " is: " +ascii2);
+    }
+}
