30980ba36293f7a78911fbca8deea377d7779a7f
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 22:15:04 +0530

    Create main1.java

---
+++ arrays/main1.java
@@ -0,0 +1,9 @@
+package com.Kluniversity;
+
+public class main1 {
+    public static void main(String[]args){
+        int[] marks={97,98,95};
+        int [][] finalmarks={{97,98,95},{95,95,98}};
+        System.out.println(finalmarks[1][2]);
+    }
+}
