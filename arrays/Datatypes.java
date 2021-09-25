6d943fbd83642105be05b38585642889a1f03771
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 20:11:18 +0530

    Create Datatypes.java

---
+++ arrays/Datatypes.java
@@ -0,0 +1,32 @@
+package com.Kluniversity;
+
+public class Datatypes {
+    public static void main(String[]args){
+        //Primitive type
+        //byte-1 [-128 to 127]
+        //short-2
+        //long-8
+        //float-4 [1.23,2.34]
+        //double-8
+        //char-2 [a,B,c]
+        //boolean-1[True/False]
+        //int-4 [1,2,3]
+        byte age = 19;
+        int phone = 901433989;
+        long phone2=9014339897L;
+        float pi=3.12F;
+        char letter='M';
+        boolean isAdult=true;
+        System.out.println(age);
+        System.out.println(phone);
+        System.out.println(phone2);
+        System.out.println(pi);
+        System.out.println(letter);
+        System.out.println(isAdult);
+        //Non-primitive types
+        String name="Mounish";
+        System.out.println(name.length());
+
+
+    }
+}
