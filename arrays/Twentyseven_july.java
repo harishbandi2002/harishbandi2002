e4b9120bd9f9779671dce9c10680b08b635db42a
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 22:10:34 +0530

    Create Twentyseven_july.java

---
+++ arrays/Twentyseven_july.java
@@ -0,0 +1,16 @@
+//Program on basic operations
+public class Twentyseven_july {
+	public static void main(String args[])
+	{
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		System.out.println("The value of a is"+a);
+		System.out.println("The value of b is"+b);
+		System.out.println("The value of a+b is"+(a+b));
+		System.out.println("The value of a-b is"+(a-b));
+		System.out.println("The value of a*b is"+(a*b));
+		System.out.println("The value of a/b is"+(a/b));
+		System.out.println("The value of a%b is"+(a%b));
+	}
+
+}
