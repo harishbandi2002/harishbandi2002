61f038e0830bf5f4498b543a88ad4f1321646854
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 22:17:24 +0530

    Create prime_not.java

---
+++ arrays/prime_not.java
@@ -0,0 +1,18 @@
+package com.java.java;
+
+public class prime_not {
+    public static void main(String[] args) {
+    int n = 29;
+    boolean flag = false;
+    for (int i = 2; i <= n / 2; ++i) {
+        if (n % i == 0) {
+            flag = true;
+            break;
+        }
+    }
+    if (!flag)
+            System.out.println(n + " is a prime number.");
+    else
+            System.out.println(n + " is not a prime number.");
+}
+}
