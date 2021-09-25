b77b43791539b3303fa894a4a8c9eddcdf382d11
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 22:12:26 +0530

    Create cha.java

---
+++ arrays/cha.java
@@ -0,0 +1,9 @@
+public record cha() {
+    public static void main(String[] args) {
+        char ch,ch1;
+        ch=args[0].charAt(0);
+        ch1=args[1].charAt(1);
+        System.out.println(ch);
+        System.out.println(ch1);
+    }
+}
