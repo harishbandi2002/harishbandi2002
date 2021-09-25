a8dc85677ddb4c2c9460e6bbef9145c5976c0d06
harishbandi2002 <88489116+harishbandi2002@users.noreply.github.com>
2021-09-25 22:09:32 +0530

    Create String3.java

---
+++ arrays/String3.java
@@ -0,0 +1,25 @@
+public class String3{
+    static boolean isSubSequence(String str1, String str2,int m, int n)
+    {
+        if (m == 0)
+            return true;
+        if (n == 0)
+            return false;
+        if (str1.charAt(m - 1) == str2.charAt(n - 1))
+            return isSubSequence(str1, str2, m - 1, n - 1);
+        return isSubSequence(str1, str2, m, n - 1);
+    }
+    public static void String3(String[] args)
+    {
+        String str1 = "axc";
+        String str2 = "ahbgdc";
+        int m = str1.length();
+        int n = str2.length();
+        boolean res = isSubSequence(str1, str2, m, n);
+        if (res)
+            System.out.println("True");
+        else
+            System.out.println("false");
+    }
+
+ 
